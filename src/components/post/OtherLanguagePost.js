17a891527a79a495000669e877da918a728b0643
Jénaïc Cambré a.k.a kadiks <me@jenaiccambre.com>
2018-01-07 11:13:08 +0100

    fix non journal post display

--- src/components/post/OtherLanguagePost.js
+++ src/components/post/OtherLanguagePost.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import moment from 'moment';
+import filterPost from '../../utils/filters/Post';
+import FlagIcon from "../flags/FlagIcon";
 
 export default class OtherPost extends React.Component {
     isFrenchPost({ post }) {
@@ -14,32 +16,34 @@ export default class OtherPost extends React.Component {
         const availableText = language === "en" ?
             "Article disponible en français : " :
             "Post available in English: ";
-        const isLookingForFrenchPost = language === "en" ? true : false;
-        let curPost = null;
-        const postTimestamp = Number(moment(post.date).startOf('day').format('x'));
+        // const isLookingForFrenchPost = language === "en" ? true : false;
+        // let curPost = null;
+        // const postTimestamp = Number(moment(post.date).startOf('day').format('x'));
 
-        posts.edges.forEach(({ node }) => {
-            let tags = node.tags || [];
-            tags = tags.map(t => t.name);
+        // posts.edges.forEach(({ node }) => {
+        //     let tags = node.tags || [];
+        //     tags = tags.map(t => t.name);
 
-            const date = moment(node.date).startOf('day');
-            node.dateObject = {
-                m: date.format("MM"),
-                y: date.format("YYYY"),
-                d: date.format("DD")
-              };
+        //     const date = moment(node.date).startOf('day');
+        //     node.dateObject = {
+        //         m: date.format("MM"),
+        //         y: date.format("YYYY"),
+        //         d: date.format("DD")
+        //       };
             
-            const timestamp = Number(moment(node.date).startOf('day').format('x'));
+        //     const timestamp = Number(moment(node.date).startOf('day').format('x'));
 
-            if (postTimestamp === timestamp) {
-                // console.log('node', node);
-                // console.log('this.isFrenchPost({ post: node })', this.isFrenchPost({ post: node }));
-                // console.log('isLookingForFrenchPost', isLookingForFrenchPost);
-                if (this.isFrenchPost({ post: node }) === isLookingForFrenchPost) {
-                    curPost = node;
-                }
-            }
-        });
+        //     if (postTimestamp === timestamp) {
+        //         // console.log('node', node);
+        //         // console.log('this.isFrenchPost({ post: node })', this.isFrenchPost({ post: node }));
+        //         // console.log('isLookingForFrenchPost', isLookingForFrenchPost);
+        //         if (this.isFrenchPost({ post: node }) === isLookingForFrenchPost) {
+        //             curPost = node;
+        //         }
+        //     }
+        // });
+
+        const curPost = filterPost.getJournalPostInOtherLanguage({ post, posts });
 
         // console.log('cmp/post/OtherLanguagePost curPost', curPost);
 
@@ -50,8 +54,8 @@ export default class OtherPost extends React.Component {
         const link = `${language === "en" ? "fr" : "en"}/${curPost.dateObject.y}/${curPost.dateObject.m}/${curPost.dateObject.d}/${curPost.slug}`;
         return (
             <p>
-                <small>
-                    {availableText}<Link to={link}>{curPost.title}</Link>
+                <small style={{ fontStyle: "italic" }}>
+                    <FlagIcon code={language === "en" ? "fr" : "gb"} /> {availableText}<Link to={link}>{curPost.title}</Link>
                 </small>
             </p>
         );
